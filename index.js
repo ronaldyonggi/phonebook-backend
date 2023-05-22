e75254939bad6c24e0a985cebcada65aed08e553
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-05-21 20:07:15 -0400

    3.7-3.8 completed

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
 const PORT = 3001
 
+// Create a new token for morgan
+morgan.token('body', (req, res) => {
+  return JSON.stringify(req.body)
+})
+
 app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
   { 
