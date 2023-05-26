61d1de8371aad6a91989764e6e1bdf09871f9dfb
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-05-26 13:12:58 -0400

    removed local data and generate ID function

--- index.js
+++ index.js
@@ -14,34 +14,6 @@ app.use(express.static('build'))
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-let persons = [
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-]
-
-// Generate a random integer between between 100 to 9999
-const generateRandomInt = () => {
-  return Math.floor(Math.random() * (9990 - 100) + 100) 
-}
-
 // Home page route
 app.get('/', (request, response) => {
   response.send('<h1>Homepage</h1>')
