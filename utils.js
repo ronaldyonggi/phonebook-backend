3b311e4992ef5904600f60d2ef2af994494655e8
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-05-28 15:05:59 -0400

    removed utils

--- utils.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-// Checks if id is a valid MongoDB object ID
-const isValidId = id => {
-  return mongoose.Types.ObjectId.isValid(id)
-}
-
-module.exports = isValidId
