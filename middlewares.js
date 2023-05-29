0cfd08dbe133ed814e3edb88a632961bf1ca1239
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-05-29 13:12:52 -0400

    3.22 eslint style applied

--- middlewares.js
+++ middlewares.js
@@ -3,9 +3,9 @@ const errorHandler = (error, request, response, next) => {
   console.error(error.message)
 
   if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'bad id format'})
+    return response.status(400).send({ error: 'bad id format' })
   } else if (error.name === 'ValidationError') {
-    return response.status(400).json({error: error.message})
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
@@ -13,7 +13,7 @@ const errorHandler = (error, request, response, next) => {
 
 // Unknown endpoint middleware
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({error: 'unknown endpoint'})
+  response.status(404).send({ error: 'unknown endpoint' })
 }
 
-module.exports = { errorHandler, unknownEndpoint}
+module.exports = { errorHandler, unknownEndpoint }
